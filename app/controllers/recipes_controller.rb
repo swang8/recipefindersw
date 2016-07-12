59870c3a803616904f48c3bd5f72958b13dfb1fb
Shichen Wang <shichen.wang@gmail.com>
2016-07-12 15:40:17 -0500

    color and typo

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    @search = params[:search] || 'mocha'
+    @search = params[:search] || 'chicken,cheese'
     @recipes = Recipe.for(@search)
     ##@fields = %w{publisher f2f_url title source_url recipe_id image_url social_rank publisher_url}
     @fields = %w{image_url title f2f_url}
